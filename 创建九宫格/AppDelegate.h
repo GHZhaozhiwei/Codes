3de7fe17393a27d0ce7b8cf688b368985700d13a
apple <857046230@163.com>
2016-08-16 17:43:26 +0800

    Initial Commit

---
+++ 创建九宫格/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  创建九宫格
+//
+//  Created by apple on 16/8/16.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
